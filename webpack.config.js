f0d075613401ac7cf0f77bce679684fc73364b44
bozoputer <e@ekathuria.dev>
2022-07-03 13:55:27 -0400

    Add support for loading SVGs

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
 		]
 	},
   resolve: {
