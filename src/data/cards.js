656732fe8e328d5a75f0c22a28dc60d436429a89
bozoputer <e@ekathuria.dev>
2022-07-03 18:50:05 -0400

    Reorganize data

---
+++ src/data/cards.js
@@ -0,0 +1,426 @@
+import club01 from './../img/cards/club01.svg';
+import club02 from './../img/cards/club02.svg';
+import club03 from './../img/cards/club03.svg';
+import club04 from './../img/cards/club04.svg';
+import club05 from './../img/cards/club05.svg';
+import club06 from './../img/cards/club06.svg';
+import club07 from './../img/cards/club07.svg';
+import club08 from './../img/cards/club08.svg';
+import club09 from './../img/cards/club09.svg';
+import club10 from './../img/cards/club10.svg';
+import club11 from './../img/cards/club11.svg';
+import club12 from './../img/cards/club12.svg';
+import club13 from './../img/cards/club13.svg';
+import heart01 from './../img/cards/heart01.svg';
+import heart02 from './../img/cards/heart02.svg';
+import heart03 from './../img/cards/heart03.svg';
+import heart04 from './../img/cards/heart04.svg';
+import heart05 from './../img/cards/heart05.svg';
+import heart06 from './../img/cards/heart06.svg';
+import heart07 from './../img/cards/heart07.svg';
+import heart08 from './../img/cards/heart08.svg';
+import heart09 from './../img/cards/heart09.svg';
+import heart10 from './../img/cards/heart10.svg';
+import heart11 from './../img/cards/heart11.svg';
+import heart12 from './../img/cards/heart12.svg';
+import heart13 from './../img/cards/heart13.svg';
+import spade01 from './../img/cards/spade01.svg';
+import spade02 from './../img/cards/spade02.svg';
+import spade03 from './../img/cards/spade03.svg';
+import spade04 from './../img/cards/spade04.svg';
+import spade05 from './../img/cards/spade05.svg';
+import spade06 from './../img/cards/spade06.svg';
+import spade07 from './../img/cards/spade07.svg';
+import spade08 from './../img/cards/spade08.svg';
+import spade09 from './../img/cards/spade09.svg';
+import spade10 from './../img/cards/spade10.svg';
+import spade11 from './../img/cards/spade11.svg';
+import spade12 from './../img/cards/spade12.svg';
+import spade13 from './../img/cards/spade13.svg';
+import diamond01 from './../img/cards/diamond01.svg';
+import diamond02 from './../img/cards/diamond02.svg';
+import diamond03 from './../img/cards/diamond03.svg';
+import diamond04 from './../img/cards/diamond04.svg';
+import diamond05 from './../img/cards/diamond05.svg';
+import diamond06 from './../img/cards/diamond06.svg';
+import diamond07 from './../img/cards/diamond07.svg';
+import diamond08 from './../img/cards/diamond08.svg';
+import diamond09 from './../img/cards/diamond09.svg';
+import diamond10 from './../img/cards/diamond10.svg';
+import diamond11 from './../img/cards/diamond11.svg';
+import diamond12 from './../img/cards/diamond12.svg';
+import diamond13 from './../img/cards/diamond13.svg';
+
+import spade from './../img/spade.svg';
+import club from './../img/club.svg';
+import diamond from './../img/diamond.svg';
+import heart from './../img/heart.svg';
+
+const cards = [
+  {
+    id: 1,
+    cover: club01,
+    pip: "Ace",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 2,
+    cover: club02,
+    pip: "Two",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 3,
+    cover: club03,
+    pip: "Three",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 4,
+    cover: club04,
+    pip: "Four",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 5,
+    cover: club05,
+    pip: "Five",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 6,
+    cover: club06,
+    pip: "Six",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 7,
+    cover: club07,
+    pip: "Seven",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 8,
+    cover: club08,
+    pip: "Eight",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 9,
+    cover: club09,
+    pip: "Nine",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 10,
+    cover: club10,
+    pip: "Ten",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 11,
+    cover: club11,
+    pip: "Jack",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 12,
+    cover: club12,
+    pip: "Queen",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 13,
+    cover: club13,
+    pip: "King",
+    suit: "clubs",
+    icon: club
+  },
+  {
+    id: 14,
+    cover: heart01,
+    pip: "Ace",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 15,
+    cover: heart02,
+    pip: "Two",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 16,
+    cover: heart03,
+    pip: "Three",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 17,
+    cover: heart04,
+    pip: "Four",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 18,
+    cover: heart05,
+    pip: "Five",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 19,
+    cover: heart06,
+    pip: "Six",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 20,
+    cover: heart07,
+    pip: "Seven",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 21,
+    cover: heart08,
+    pip: "Eight",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 22,
+    cover: heart09,
+    pip: "Nine",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 23,
+    cover: heart10,
+    pip: "Ten",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 24,
+    cover: heart11,
+    pip: "Jack",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 25,
+    cover: heart12,
+    pip: "Queen",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 26,
+    cover: heart13,
+    pip: "King",
+    suit: "hearts",
+    icon: heart
+  },
+  {
+    id: 27,
+    cover: spade01,
+    pip: "Ace",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 28,
+    cover: spade02,
+    pip: "Two",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 29,
+    cover: spade03,
+    pip: "Three",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 30,
+    cover: spade04,
+    pip: "Four",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 31,
+    cover: spade05,
+    pip: "Five",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 32,
+    cover: spade06,
+    pip: "Six",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 33,
+    cover: spade07,
+    pip: "Seven",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 34,
+    cover: spade08,
+    pip: "Eight",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 35,
+    cover: spade09,
+    pip: "Nine",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 36,
+    cover: spade10,
+    pip: "Ten",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 37,
+    cover: spade11,
+    pip: "Jack",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 38,
+    cover: spade12,
+    pip: "Queen",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 39,
+    cover: spade13,
+    pip: "King",
+    suit: "spades",
+    icon: spade
+  },
+  {
+    id: 40,
+    cover: diamond01,
+    pip: "Ace",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 41,
+    cover: diamond02,
+    pip: "Two",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 42,
+    cover: diamond03,
+    pip: "Three",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 43,
+    cover: diamond04,
+    pip: "Four",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 44,
+    cover: diamond05,
+    pip: "Five",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 45,
+    cover: diamond06,
+    pip: "Six",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 46,
+    cover: diamond07,
+    pip: "Seven",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 47,
+    cover: diamond08,
+    pip: "Eight",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 48,
+    cover: diamond09,
+    pip: "Nine",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 49,
+    cover: diamond10,
+    pip: "Ten",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 50,
+    cover: diamond11,
+    pip: "Jack",
+    suit: "diamonds",
+    icon: diamond
+  },
+  {
+    id: 51,
+    cover: diamond12,
+    suit: "diamonds",
+    pip: "Queen",
+    icon: diamond
+  },
+  {
+    id: 52,
+    cover: diamond13,
+    pip: "King",
+    suit: "diamonds",
+    icon: diamond
+  },
+];
+
+export default cards;
