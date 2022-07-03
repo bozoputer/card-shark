e4626fa4337ef4e94019655920b28b25c47eaa03
bozoputer <e@ekathuria.dev>
2022-07-03 18:53:06 -0400

    Add more styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 import title from './img/header.svg';
-import cards from './images';
+import cards from './data/cards';
 import Card from './Card';
 
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
       <main className="container mx-auto text-center flex flex-row flex-wrap">
       <section className="cards flex flex-row flex-wrap">
         { cards.map((card, index) => {
-            return <Card front={ card } alt={ `card${ index }` } key={ index } />
+            return <Card face={ card.cover } alt={ `${ card.pip } of ${ card.suit }` } key={ card.id } id={ card. id } pip={ card.pip } icon={ card.icon} />
         }) }
       </section>
       </main>
