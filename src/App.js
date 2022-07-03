f7d8f9d319c5a4b9b3ba19984b3dcd9d224f97cb
bozoputer <e@ekathuria.dev>
2022-07-03 13:57:16 -0400

    Set up card component and add basic styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import './styles.css';
-import title from './img/card-shark.png';
+import title from './img/header.svg';
 import cards from './images';
 import Card from './Card';
 
-console.log(cards);
-
 const App = () => {
+
   return (
-    <div className="wrapper">
-      <header className="container mx-auto text-center">
-        <img className="mx-auto" src={ title } alt="Card Shark" />
+    <>
+      <header className="container mx-auto text-center my-8">
+        <img className="mx-auto header-img" src={ title } alt="Card Shark logo" />
       </header>
-      <main className="container mx-auto text-center border-2 border-blue-700 flex flex-row flex-wrap">
+      <main className="container mx-auto text-center flex flex-row flex-wrap">
       <section className="cards flex flex-row flex-wrap">
         { cards.map((card, index) => {
             return <Card front={ card } alt={ `card${ index }` } key={ index } />
         }) }
       </section>
       </main>
-    </div>
+    </>
   );
 };
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -4,13 +4,15 @@ import back from "./img/cards/card-back.png";
 
 const Card = ({ front, alt }) => {
   return (
-    <div className="card">
-      <div className="card__inner">
-        <div className="card__front">
-          <img src={ front } alt={ alt } />
-        </div>
-        <div className="card__back">
-          <img src={ back } alt="playing card back" />
+    <div className="card-cell">
+      <div className="card">
+        <div className="card__inner">
+          <div className="card__front">
+            <img src={ back } alt="playing card back" />
+          </div>
+          <div className="card__back">
+            <img src={ front } alt={ alt } />
+          </div>
         </div>
       </div>
     </div>
